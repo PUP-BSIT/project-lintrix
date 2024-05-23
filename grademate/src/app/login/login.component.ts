09cd385c1fda04f75a260f29056a4fc56263e672
alima-kailamarie <12malvar.alimakailamarie@gmail.com>
2024-05-24 06:31:40 +0800

    add some content

--- grademate/src/app/login/login.component.ts
+++ grademate/src/app/login/login.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  templateUrl: './login.component.html'
 })
 export class LoginComponent {
   email: string = '';
@@ -12,6 +11,7 @@ export class LoginComponent {
   constructor() {}
 
   onSubmit() {
+    // For now, we'll just log the email and password
     console.log('Email:', this.email);
     console.log('Password:', this.password);
   }
