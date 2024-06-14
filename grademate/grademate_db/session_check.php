1ba9970bebb1794bd01b2fd665a6b9a6b4ec656c
mingo_edjudah <edjudah.ej@gmail.com>
2024-06-14 23:34:57 +0800

    add session

---
+++ grademate/grademate_db/session_check.php
@@ -0,0 +1,15 @@
+<?php
+/*
+include_once("database.php");
+
+session_start();
+
+if (isset($_SESSION['user'])) {
+    $data = array('loggedIn' => true, 'user' => $_SESSION['user']);
+    echo json_encode($data);
+} else {
+    $data = array('loggedIn' => false);
+    echo json_encode($data);
+}
+*/
+?>
