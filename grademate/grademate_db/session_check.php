b4a2a55306dae04b456ec0d3ef699a3426839fe8
mingo_edjudah <edjudah.ej@gmail.com>
2024-06-22 11:08:31 +0800

    add session check

--- grademate/grademate_db/session_check.php
+++ grademate/grademate_db/session_check.php
@@ -1,15 +1,15 @@
 <?php
-/*
 include_once("database.php");
-
 session_start();
 
+header("Access-Control-Allow-Origin: http://localhost:4200");
+header("Access-Control-Allow-Credentials: true");
+header("Content-Type: application/json; charset=UTF-8");
+
 if (isset($_SESSION['user'])) {
-    $data = array('loggedIn' => true, 'user' => $_SESSION['user']);
-    echo json_encode($data);
+    $response = array('message' => 'active', 'user' => $_SESSION['user']);
 } else {
-    $data = array('loggedIn' => false);
-    echo json_encode($data);
+    $response = array('message' => 'inactive');
 }
-*/
+echo json_encode($response);
 ?>
