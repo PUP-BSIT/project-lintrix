8b86b3ac61cd0a7e20512a1de0d02fd9c31fc786
mingo_edjudah <edjudah.ej@gmail.com>
2024-06-28 21:32:44 +0800

    add signup api

--- grademate/src/app/services/signup.service.ts
+++ grademate/src/app/services/signup.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class SignupService {
+  private signupUrl = 'http://localhost/php-project';
+  //private signupUrl = 'https://grademate.tech/api';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  signup(data: any): Observable<any> {
+    return this.http.post<any>(`${this.signupUrl}/signup.php`, data, { observe: 'response' });
+  }
 }
