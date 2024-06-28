01b619fa41e532a39535acaec5050999b6288457
mingo_edjudah <edjudah.ej@gmail.com>
2024-06-28 21:32:54 +0800

    add student api

--- grademate/src/app/services/student.service.ts
+++ grademate/src/app/services/student.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class StudentService {
+  private apiUrl = 'http://localhost/php-project/student.php';
+  //private apiUrl = 'https://grademate.tech/api/student.php';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getStudent(): Observable<any> {
+    return this.http.get(this.apiUrl, { withCredentials: true });
+  }
+
+  createStudent(data: any): Observable<any> {
+    return this.http.post(this.apiUrl, data, { withCredentials: true });
+  }
+
+  updateStudent(data: any): Observable<any> {
+    return this.http.put(this.apiUrl, data, { withCredentials: true });
+  }
+
+  deleteStudent(id: number): Observable<any> {
+    const url = `${this.apiUrl}?id=${id}`;
+    return this.http.delete(url, { withCredentials: true });
+  }
 }
